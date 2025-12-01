29e7149b1439fd11e8815fd8d1d4243023ff07ab
Karim Ali <kareemaly.2004@gmail.com>
2025-12-01 23:25:15 +0200

    test endpoint

---
+++ src/EaseClub.Api/Controllers/TestContoller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EaseClub.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        // GET: /api/test
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Message = "EaseClub API is working!",
+                Time = DateTime.UtcNow
+            });
+        }
+    }
+}
