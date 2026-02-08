b9ddd34cf7c08f63793fb3685bcafcffeaedac8e
Karim Ali <kareemaly.2004@gmail.com>
2026-02-08 19:19:49 +0300

    refator(domain,installmentTemplate):add xml doc for create factory method

--- src/EaseClub.Domain/MembershipPlans/InstallmentTemplate.cs
+++ src/EaseClub.Domain/MembershipPlans/InstallmentTemplate.cs
@@ -33,6 +33,42 @@ namespace EaseClub.Domain.MembershipPlans
             ClubId = clubId;
         }
 
+    #region xml-doc-about create factory method
+        /// <summary>
+        /// Factory method for creating an <see cref="InstallmentTemplate"/>.
+        ///
+        /// The template can be created in one of two ways:
+        ///
+        /// 1. <b>Auto-generated installments</b>:
+        ///    If both <paramref name="numOfInstallments"/> and <paramref name="durationInDays"/> are provided,
+        ///    the installments will be generated automatically based on these values.
+        ///    In this case, any provided <paramref name="installments"/> list will be ignored.
+        ///
+        /// 2. <b>Manual installments</b>:
+        ///    If <paramref name="numOfInstallments"/> and <paramref name="durationInDays"/> are not provided,
+        ///    a custom template can be created by supplying a non-empty <paramref name="installments"/> list.
+        ///
+        /// If neither a valid number/duration nor a valid installments list is provided,
+        /// the creation will fail with a domain error.
+        /// </summary>
+        /// <param name="id">The unique identifier of the installment template.</param>
+        /// <param name="clubId">The club to which this template belongs.</param>
+        /// <param name="name">The name of the installment template.</param>
+        /// <param name="numOfInstallments">
+        /// Number of installments to auto-generate. Must be provided together with <paramref name="durationInDays"/>.
+        /// </param>
+        /// <param name="durationInDays">
+        /// Total duration (in days) over which the installments are distributed.
+        /// Must be provided together with <paramref name="numOfInstallments"/>.
+        /// </param>
+        /// <param name="installments">
+        /// A custom list of installments. Used only when auto-generation parameters are not provided.
+        /// </param>
+        /// <returns>
+        /// A <see cref="Result{InstallmentTemplate}"/> containing the created template
+        /// or a domain error if validation fails.
+        /// </returns>
+    #endregion 
         public static Result<InstallmentTemplate>Create(Guid id,Guid clubId,string name,int? numOfInstallments,int? durationInDays,
             List<Installment>? installments)
         {
