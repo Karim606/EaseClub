5a95ba38374acef546d5e2ebf6b1ed272afa5a77
Karim Ali <kareemaly.2004@gmail.com>
2026-03-11 01:45:48 +0300

    feat(app):add common abstract base class for domain event handling

---
+++ src/EaseClub.Application/Common/DomainEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaseClub.Application.Common
+{
+    public abstract class DomainEventHandler<TEvent> : INotificationHandler<TEvent>
+    where TEvent : DomainEvent
+    {
+        //protected readonly INotificationService _notificationService;
+        //protected readonly IAuditLogger _auditLogger;
+
+        //public DomainEventHandler(INotificationService notificationService,
+        //                          IAuditLogger auditLogger)
+        //{
+        //    _notificationService = notificationService;
+        //    _auditLogger = auditLogger;
+        //}
+
+        public abstract Task Handle(TEvent evt, CancellationToken ct);
+
+        //protected async Task LogEventAsync(TEvent evt)
+        //{
+        //    await _auditLogger.LogAsync(evt.EventId, evt.GetType().Name, evt.OccurredOn);
+        //}
+
+        //protected async Task NotifyUserAsync(Guid userId, string message)
+        //{
+        //    await _notificationService.SendAsync(userId, message);
+        //}
+    }
+}
