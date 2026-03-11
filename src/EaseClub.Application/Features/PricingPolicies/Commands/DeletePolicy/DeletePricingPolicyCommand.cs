f0a8146389e0af38b2858c573255fdb447f3a832
Karim Ali <kareemaly.2004@gmail.com>
2026-03-11 20:42:36 +0300

    feat(pricing policy) :implement create,update and delete command and their handlers

---
+++ src/EaseClub.Application/Features/PricingPolicies/Commands/DeletePolicy/DeletePricingPolicyCommand.cs
@@ -0,0 +1,24 @@
+﻿using EaseClub.Application.Common;
+using EaseClub.Application.Common.Interfaces;
+using EaseClub.Domain.Common.Results;
+using EaseClub.Domain.PricingPolices;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EaseClub.Application.Features.PricingPolicies.Commands.DeletePolicy
+{
+    public record DeletePricingPolicyCommand(Guid Id) : IRequest<Result<Success>>, IRequireClubOwnershipValidation
+    {
+        public IEnumerable<OwnershipRule> Rules()
+        {
+            yield return new OwnershipRule(
+                (auth,clubId) => auth.DoesResourceBelongToClubAsync<PricingPolicy>(Id,clubId),
+                nameof(PricingPolicies),
+                Id);
+        }
+    }
+}
