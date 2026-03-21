dfcc7167c5373ea7f674b2bdc0cf6c4e12f67c76
Karim Ali <kareemaly.2004@gmail.com>
2026-03-21 22:13:42 +0300

    feat(migartion):add migration

---
+++ src/EaseClub.Infrastructure/Data/Migrations/20260321190453_add IsActive to some tables..cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EaseClub.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIsActivetosometables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "InstallmentTemplates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MembershipApplications_MemberUsers_UserId",
+                table: "MembershipApplications",
+                column: "UserId",
+                principalTable: "MemberUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MembershipApplications_MemberUsers_UserId",
+                table: "MembershipApplications");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "InstallmentTemplates");
+        }
+    }
+}
