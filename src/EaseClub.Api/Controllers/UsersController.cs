066db8b0b287c96fef1c0e654176a45b33764c84
Karim Ali <kareemaly.2004@gmail.com>
2026-04-26 21:00:40 +0300

    feat(api): remove memberships
    - Remove GET api/users/me/memberships endpoint and related code

--- src/EaseClub.Api/Controllers/UsersController.cs
+++ src/EaseClub.Api/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
 ﻿using EaseClub.Application.Features.Users.Queries.GetClubAdminContext;
-using EaseClub.Application.Features.Users.Queries.GetUserMemberClubs;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -11,20 +10,7 @@ namespace EaseClub.Api.Controllers
     [Authorize]
     public class UsersController(ISender sender) : ApiController
     {
-        [HttpGet("memberships")]
-        [EndpointSummary("Retrieves all clubs where the current user is a member.")]
-        [EndpointDescription("Returns a list of club IDs and names that the authenticated user is currently a member of, used for filtering applications and viewing status.")]
-        [ProducesResponseType(typeof(List<UserClubResponse>), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        public async Task<IActionResult> GetMyMemberships()
-        {
-            // Extension method to get ID from JWT Claims
-            var result = await sender.Send(new GetUserMemberClubsQuery());
-            return result.Match(
-                (val) =>Ok(val)
-                , Problem);
-        }
-
+        
         [HttpGet("admin-context")]
         [EndpointSummary("Retrieves the administrative context for the current user.")]
         [EndpointDescription("Returns the ClubAdminId and the associated ManagedClubId, providing necessary context for building administrative requests")]
