ffbdea8cfccf0774b2a3a224aef813b0e3f9a5bd
Karim Ali <kareemaly.2004@gmail.com>
2026-05-27 16:32:22 +0300

    feat(migration): add UpdateEventImageAndRegistration database migration

---
+++ src/EaseClub.Infrastructure/Data/Migrations/20260527133210_UpdateEventImageAndRegistration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EaseClub.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEventImageAndRegistration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Events");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageId",
+                table: "Events",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReadableId",
+                table: "EventRegistrations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ImageId",
+                table: "Events",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_FileResources_ImageId",
+                table: "Events",
+                column: "ImageId",
+                principalTable: "FileResources",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_FileResources_ImageId",
+                table: "Events");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ImageId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ReadableId",
+                table: "EventRegistrations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Events",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+    }
+}
