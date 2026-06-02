4ab3638729861eeb013506979a00b05ed4aef78f
Karim Ali <kareemaly.2004@gmail.com>
2026-06-02 22:39:34 +0300

    refactor(database): Remove age and gender restrictions
    - Remove `GenderRestriction`, `MaxAge`, and `MinAge` columns from the `TicketTypes` table.
    
    BREAKING CHANGE: Removes age and gender restrictions from `TicketTypes`, potentially altering ticket eligibility logic.

---
+++ src/EaseClub.Infrastructure/Data/Migrations/20260602193804_event update.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EaseClub.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class eventupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GenderRestriction",
+                table: "TicketTypes");
+
+            migrationBuilder.DropColumn(
+                name: "MaxAge",
+                table: "TicketTypes");
+
+            migrationBuilder.DropColumn(
+                name: "MinAge",
+                table: "TicketTypes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GenderRestriction",
+                table: "TicketTypes",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxAge",
+                table: "TicketTypes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinAge",
+                table: "TicketTypes",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
